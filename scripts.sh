39fa1778f670b1fe8767f25955d331211198900d
Suresh Alse <alseambusher@gmail.com>
2012-12-04 06:39:59 +0530

    vars

--- scripts.sh
+++ scripts.sh
@@ -5,9 +5,15 @@
 #echo superglobals
 #get supeglobals
 #cat index.php |grep '\$_'
+# --exclude vars,functions,classes,system vars,supergblobals
 #TODO fix this
 for i in `ls *.php`
 do
     #echo "<?include('$i');echo '#####';print_r(get_defined_vars());?>"|php5
-    echo "<?include('$i');echo '#####';\$a=get_defined_functions();print_r(\$a['user']);?>"|php5
+    echo "<?ob_start();
+    include('$i');
+    ob_end_clean();
+    \$a=get_defined_functions();
+    print_r(\$a['user']);?>"|php5 2>/dev/null
+    #TODO get all the functions,classes,user vars,superglobals
 done
