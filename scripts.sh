eae10383a39c30a804502ec168c04a5ece5ee327
Suresh Alse <alseambusher@gmail.com>
2012-12-07 16:19:48 +0530

    some

--- scripts.sh
+++ scripts.sh
@@ -6,14 +6,63 @@
 #get supeglobals
 #cat index.php |grep '\$_'
 # --exclude vars,functions,classes,system vars,supergblobals
-#TODO fix this
-for i in `ls *.php`
-do
-    #echo "<?include('$i');echo '#####';print_r(get_defined_vars());?>"|php5
-    echo "<?ob_start();
-    include('$i');
+exvars=0
+exfuncs=0
+allfuncs=0
+function get_user_func {
+        #echo "<?include('$i');echo '#####';print_r(get_defined_vars());?>"|php5
+    echo "<?
+    ob_start();
+    include('$file');
     ob_end_clean();
     \$a=get_defined_functions();
-    print_r(\$a['user']);?>"|php5 2>/dev/null
+    \$a=\$a['user'];
+    \$isempty=0;
+    foreach(\$a as \$row){
+        echo \$row.'<br>';
+        \$isempty=1;
+    }
+    if(\$isempty==0)
+        echo 'None';
+    ?>"|php5 2>>yasg.log
     #TODO get all the functions,classes,user vars,superglobals
-done
+}
+function get_all_func {
+        #echo "<?include('$i');echo '#####';print_r(get_defined_vars());?>"|php5
+    echo "<?ob_start();
+    include('$file');
+    ob_end_clean();
+    foreach(get_defined_functions() as \$row){
+    foreach(\$row as \$row2)
+        echo \$row2.'<br>';
+    }?>"|php5 2>>yasg.log
+    #TODO get all the functions,classes,user vars,superglobals
+}
+function get_included_files {
+    echo "<? ob_start();
+    include('$file');
+    ob_end_clean();
+    foreach(get_included_files() as \$row){
+        echo \$row.'<br>';
+    }?>"|php5 2>>yasg.log
+}
+function get_constants {
+    echo "<?
+    ob_start();
+    include('$file');
+    ob_end_clean();
+    \$a=get_defined_constants();
+    if(isset(\$a['user']))
+    \$a=\$a['user'];
+    else
+    \$a=array();
+    \$isempty=0;
+    foreach(\$a as \$row){
+        echo \$row.'<br>';
+        \$isempty=1;
+    }
+    if(\$isempty==0)
+        echo 'None';
+    ?>"|php5 2>>yasg.log
+   
+}
